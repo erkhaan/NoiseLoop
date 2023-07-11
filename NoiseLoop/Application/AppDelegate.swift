1e82f0a18c1ebc6e2f40341dfdb538fccdafafd0
erkhaan <notykt@gmail.com>
2023-07-11 22:46:06 +0600

    [add] audio session setup

--- NoiseLoop/Application/AppDelegate.swift
+++ NoiseLoop/Application/AppDelegate.swift
@@ -10,7 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        AudioSessionService.setupAudioSession()
         return true
     }
 
